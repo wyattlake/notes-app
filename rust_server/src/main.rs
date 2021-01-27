89e950a196f27f1e621f4d6e8222224ccec5aca7
Wyatt Lake <wattttte@gmail.com>
2021-01-26 22:34:01 -0800

    Added experimental Rust backend

---
+++ rust_server/src/main.rs
@@ -0,0 +1,20 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    const PORT: u16 = 8080;
+    println!("Starting server at port {}", PORT);
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind(("127.0.0.1", PORT))?
+    .run()
+    .await
+}
